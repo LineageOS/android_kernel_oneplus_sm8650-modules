a2b087554da8079d0112a132c128c871963dbef5
inferno0230 <mail@inferno0230.in>
2025-09-30 23:42:06 +0530

    display-drivers: Import additional changes from OPD2403_15.0.0.1101(EX01)
    Change-Id: I1e0ab1776618dabf932de049d81c7621180cee38
    Signed-off-by: inferno0230 <mail@inferno0230.in>

--- qcom/opensource/display-drivers/msm/dsi/dsi_panel.c
+++ qcom/opensource/display-drivers/msm/dsi/dsi_panel.c
@@ -80,6 +80,8 @@
 
 #ifdef OPLUS_FEATURE_DISPLAY
 static bool g_oplus_forced_power_down = false;
+bool caihong_panel_flag = false;
+EXPORT_SYMBOL(caihong_panel_flag);
 #endif /* OPLUS_FEATURE_DISPLAY */
 
 extern bool g_gamma_regs_read_done;
@@ -1074,7 +1076,9 @@ int dsi_panel_set_backlight(struct dsi_panel *panel, u32 bl_lvl)
 		return 0;
 
 #if IS_ENABLED(CONFIG_OPLUS_POWER_NOTIFIER)
-	if (g_oplus_forced_power_down) {
+	if (!strcmp(panel->name, "Dual dsi csot nt36532 video mode panel with DSC")
+			|| !strcmp(panel->name, "Dual dsi nt36523w video mode panel with DSC")
+            || g_oplus_forced_power_down) {
 		if (panel->pon_status == OPLUS_PON_KPDPWR_RESIN_BARK) {
 			DSI_ERR("%s: %d: pon_status is OPLUS_PON_KPDPWR_RESIN_BARK, return\n", __func__, __LINE__);
 			return 0;
@@ -4703,34 +4707,58 @@ static int oplus_power_notifier_callback(struct notifier_block *self, unsigned l
 
 	if (event == OPLUS_POWER_EVENT_PON) {
 		if (evdata->pon_status == OPLUS_PON_KPDPWR_RESIN_BARK) {
-			rc = dsi_panel_update_backlight(panel, 0);
-			panel->pon_status = OPLUS_PON_KPDPWR_RESIN_BARK;
+            if(is_project(23926) || is_project(23927) || is_project(23976) || is_project(23978)) {
+                bl_ic_ktz8866_set_brightness(0);
 
-			if (rc < 0)
-				DSI_ERR("failed to update backlight: rc = %d\n", rc);
+                mutex_lock(&panel->panel_lock);
+                dsi_panel_tx_cmd_set(panel, DSI_CMD_SET_OFF);
+                mutex_unlock(&panel->panel_lock);
 
-			msleep(5);
+                gpio_set_value(panel->reset_config.reset_gpio, 0);
+                DSI_INFO("AVDD/AVEE false\n");
+                bl_ic_ktz8866_set_lcd_bias_by_gpio(false);
+                usleep_range(5*1000, (5*1000)+100);
+                bl_ic_ktz8866_hw_en(false);
+                usleep_range(15*1000, (15*1000)+100);
+                DSI_INFO("VDDI lower\n");
+                dsi_pwr_enable_regulator(&panel->power_info, false);
+                panel->pon_status = OPLUS_PON_KPDPWR_RESIN_RELEASE;
+            } else {
+                rc = dsi_panel_update_backlight(panel, 0);
+                panel->pon_status = OPLUS_PON_KPDPWR_RESIN_BARK;
 
-			mutex_lock(&panel->panel_lock);
-			rc = dsi_panel_tx_cmd_set(panel, DSI_CMD_SET_OFF);
-			mutex_unlock(&panel->panel_lock);
-			if (rc < 0)
-				DSI_ERR("failed to dsi_panel_tx_cmd_set DSI_CMD_SET_OFF: rc = %d\n", rc);
+                if (rc < 0)
+                    DSI_ERR("failed to update backlight: rc = %d\n", rc);
 
-			DSI_ERR("OPLUS_PON_KPDPWR_RESIN_BARK wait for pon key debouncing....\n");
-			msleep(200);
+                msleep(5);
 
-			panel->pon_status = OPLUS_PON_KPDPWR_RESIN_RELEASE;
-			DSI_ERR("panel->pon_status recovered to OPLUS_PON_KPDPWR_RESIN_RELEASE....\n");
+                mutex_lock(&panel->panel_lock);
+                rc = dsi_panel_tx_cmd_set(panel, DSI_CMD_SET_OFF);
+                mutex_unlock(&panel->panel_lock);
+                if (rc < 0)
+                    DSI_ERR("failed to dsi_panel_tx_cmd_set DSI_CMD_SET_OFF: rc = %d\n", rc);
+
+                DSI_ERR("OPLUS_PON_KPDPWR_RESIN_BARK wait for pon key debouncing....\n");
+                msleep(200);
+
+                panel->pon_status = OPLUS_PON_KPDPWR_RESIN_RELEASE;
+                DSI_ERR("panel->pon_status recovered to OPLUS_PON_KPDPWR_RESIN_RELEASE....\n");
+            }
 		} else if (evdata->pon_status == OPLUS_PON_KPDPWR_RESIN_RELEASE) {
-			DSI_ERR("OPLUS_PON_KPDPWR_RESIN_RELEASE\n");
-			panel->pon_status = OPLUS_PON_KPDPWR_RESIN_RELEASE;
-			rc = dsi_panel_update_backlight(panel, bl_lvl_backup);
-		}
+            if(is_project(23926) || is_project(23927) || is_project(23976) || is_project(23978)) {
+                bl_ic_ktz8866_set_brightness(bl_lvl_backup);
+            } else {
+                DSI_ERR("OPLUS_PON_KPDPWR_RESIN_RELEASE\n");
+                panel->pon_status = OPLUS_PON_KPDPWR_RESIN_RELEASE;
+                rc = dsi_panel_update_backlight(panel, bl_lvl_backup);
+            }
+        }
 	}
 
+if(!(is_project(23926) || is_project(23927) || is_project(23976) || is_project(23978))) {
 	if (rc < 0)
 		DSI_ERR("failed to update backlight: rc = %d\n", rc);
+}
 
 	return 0;
 }
@@ -4781,9 +4809,16 @@ struct dsi_panel *dsi_panel_get(struct device *parent,
 			panel->name = "AA584 P 7 A0001 dsc cmd mode panel";
 		}
 	}
+    if (is_project(23926) || is_project(23927) || is_project(23976) || is_project(23978)) {
+		if (!strcmp(panel->name, "Dual dsi csot nt36532 video mode panel with DSC")
+			|| !strcmp(panel->name, "Dual dsi nt36523w video mode panel with DSC")) {
+			DSI_INFO("caihong_panel_flag: true\n");
+			caihong_panel_flag = true;
+        }
+    }
 #if IS_ENABLED(CONFIG_OPLUS_POWER_NOTIFIER)
-	if (g_oplus_forced_power_down) {
-		DSI_INFO("dongfeng_panel_flag: true\n");
+	if (caihong_panel_flag || g_oplus_forced_power_down) {
+		DSI_INFO("caihong/dongfeng_panel_flag: true\n");
 		panel->oplus_power_notify_client.notifier_call = oplus_power_notifier_callback;
 		rc = oplus_power_notifier_register_client(&panel->oplus_power_notify_client);
 		if (rc) {
